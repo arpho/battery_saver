6903d26ec2355360b9c33d22c4205d17ec0b7a62
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-02-26 06:05:09 +0100

    we read battery's charge
    multiplied battery.state_of_charge for 100.0, mind the number format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() -> Result<(), battery::Error> {
     let manager = battery::Manager::new()?;
+    //let percentage =
+
+ //impl Mul for uom::si::Quantity{
+    // fn Mul(fattore:i32){
+    //     println!("moltiplico per {}",fattore );
+     //}
+ //}
 
     for (idx, maybe_battery) in manager.batteries()?.enumerate() {
         let battery = maybe_battery?;
@@ -8,9 +15,12 @@ fn main() -> Result<(), battery::Error> {
         println!("Model: {:?}", battery.model());
         println!("State: {:?}", battery.state());
         println!("Time to full charge: {:?}", battery.time_to_full());
-        println!("percentuale di carica: {:?}",battery.state_of_charge());
+        println!("percentuale di carica: {:.2?}",(battery.state_of_charge()*100.0));
+
 
         println!("");
+
+
     }
 
     Ok(())
