d4b4fc3db5c14fe7b40a826e4a02299b4bb82f0a
Giuseppe D'Amico <damicogiuseppe77@gmail.com>
2021-03-15 11:43:46 +0100

    logic is ok
    we recognize whether switching off or on the current

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,39 @@
+
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+fn calculate_treshold(reference:f32,deviation:f32)-> f32{
+    return reference+ reference*deviation/100.0
+}
+use battery::units::ratio::percent;
 fn main() -> Result<(), battery::Error> {
     let manager = battery::Manager::new()?;
-    //let percentage =
+    let treshold = 50;
 
- //impl Mul for uom::si::Quantity{
-    // fn Mul(fattore:i32){
-    //     println!("moltiplico per {}",fattore );
-     //}
- //}
 
     for (idx, maybe_battery) in manager.batteries()?.enumerate() {
         let battery = maybe_battery?;
+        let charge = battery.state_of_charge()*100.0;
         println!("Battery #{}:", idx);
         println!("Vendor: {:?}", battery.vendor());
         println!("Model: {:?}", battery.model());
         println!("State: {:?}", battery.state());
         println!("Time to full charge: {:?}", battery.time_to_full());
-        println!("percentuale di carica: {:.2?}",(battery.state_of_charge()*100.0));
+        println!("percentuale di carica: {:.2?}",charge);
+        let charge = battery.state_of_charge().get::<percent>();
 
 
-        println!("");
+
+match charge {
+    charge if charge < calculate_treshold(90.0,-10.0) => println!("on"),
+    charge if charge > calculate_treshold(90.0,10.0) => println!("off"),
+    _=> println!("not covered")
+}
+
+
+
+
 
 
     }
